10787dd73c78ff6bc3caf9d9c03226705ace767b
Denys Butenko <ua13dark@gmail.com>
2016-06-06 16:27:42 +0300

    Add empty polls page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def show
+    render 'polls/show'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'polls' => 'home#show'
 end
