05c053d06186fb4edc3acc0ab4721d0060f4e089
Denys Butenko <ua13dark@gmail.com>
2016-06-08 20:08:29 +0300

    Add static_pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def show
+    render 'static_pages/konceptet'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :polls
+  resources :static_pages
 end
