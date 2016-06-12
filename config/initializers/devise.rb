0a7678ff17414e2956a1eb31097761101b280ce1
Sorenfu <sh@compfix.dk>
2016-06-12 09:55:51 +0200

    Fix fb login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.omniauth :facebook, "KEY", "SECRET"
+  config.omniauth :facebook, "880820042024209", "969c46dcc7aa2225f1e7cb6f3341badb"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
