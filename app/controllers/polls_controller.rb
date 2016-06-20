e92932cda5e7ac2c88297f67f1626fcc67decfe7
Sorenfu <sh@compfix.dk>
2016-06-20 09:27:42 +0200

    Enforce only voting once

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -14,9 +14,13 @@ class PollsController < ApplicationController
   end
 
   def vote
-    vote_option = @poll.vote_options.find(params[:vote_option_id])
-    Vote.create! user: current_user, poll: @poll, vote_option: vote_option
-    redirect_to @poll, notice: 'Din stemme blev gemt!'
+    if user_has_voted?
+      redirect_to @poll, notice: 'Du har allerede stemt i denne afstemning'
+    else
+      vote_option = @poll.vote_options.find(params[:vote_option_id])
+      Vote.create! user: current_user, poll: @poll, vote_option: vote_option
+      redirect_to @poll, notice: 'Din stemme blev gemt!'
+    end
   end
 
   private
@@ -27,5 +31,9 @@ class PollsController < ApplicationController
 
   def require_user!
     redirect_to sign_in_path(after: poll_path(@poll)) #(poll_id: @poll.id, vote_option_id: params[:vote_option_id])
+  end
+
+  def user_has_voted?
+    Vote.exists?(user_id: current_user, poll_id: @poll.id)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -16,4 +16,5 @@ class Vote < ActiveRecord::Base
   belongs_to :vote_option
 
   validates_presence_of :user, :poll, :vote_option
+  validates_uniqueness_of :user_id, scope: :poll_id
 end
